64ddbe12d075ff91d84838cd3a4f5bdb475af580
Manavi-ghorpade <ghorpademanavi1999@gmail.com>
2024-06-17 02:12:32 -0700

    added array

---
+++ array/rotated_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        vector<int> temp(nums.size()); //created another array of size of given array 
+        for(int i=0;i<nums.size();++i) 
+        {
+            int t= (i+k)%nums.size();  //calculated place to add elements 0+3 % 5 = 3 , so 0th element will be placed at 3rd place in new array
+            temp[t]=nums[i]; 
+        }
+        for(int i=0;i<nums.size();++i) //assign elements to original array again !
+        {
+            nums[i]=temp[i];
+        }
+    } 
+};  // time complexity - O(n) space complexity - O(n)
